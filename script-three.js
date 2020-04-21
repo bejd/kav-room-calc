6261310134a377e4a6c79739c29af1c0d0297d75
bejd <55742919+bejd@users.noreply.github.com>
2020-04-21 13:27:35 +0100

    More objects and options for room visualiser
    Primarily using Three.js.
    
    Fixed display being blurry on HiDPI displays.
    
    Replaced planes for walls with a cube with inner texture.
    
    Added shadows and lighting system to help visualise depth.
    
    Camera zoom changes based on floor dimensions.
    
    Added meshes for projector and lectern, with options to alter lectern size based on real data.
    
    Option for screen size. Dimensions calculated from diagonal and assuming 16:9 ratio.
    
    Added sliders to vary position of projector and screens.
    
    Option for repositioning of lectern to left or right of room.
    
    For development purposes, separated HTML options into user facing options (that will probably persist in production version) and dev-only options for testing.
    
    Separated renderer and scene creation into separate function so it's only called once: redrawing the room can still be drawn multiple times without issue.

--- script-three.js
+++ script-three.js
@@ -1,4 +1,10 @@
-var dimX, dimY, dimZ, display, visualiser;
+var dimX, dimY, dimZ;										// Room variables
+var screenType, screenWidth, screenHeight					// Screen sizes
+var pjPos, dspPos;											// Positions
+var lectX, lectY, lectZ										// Lectern
+var lectPos = "left";
+var visualiser, canvasWidth, canvasHeight, renderer, scene;	// Three.js things
+var drawRoom = false;
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -6,8 +12,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	dimX = document.getElementById("dimensions-x");
 	dimY = document.getElementById("dimensions-y");
 	dimZ = document.getElementById("dimensions-z");
+	pjPos = document.getElementById("pjPosition");
+	dspPos = document.getElementById("displayPosition");
+
 	visualiser = document.getElementById("visualiser");
-	display = document.getElementById("display");
 
 	// Listen for changes to the dimension boxes
 	dimX.addEventListener("input", function() {
@@ -22,23 +30,68 @@ document.addEventListener("DOMContentLoaded", (event) => {
 		calcSize();
 	});
 
-	dimX.value = 4; // quick testing
-	dimY.value = 4; // quick testing
-	dimZ.value = 2; // quick testing
+	// Listen for changes to radio buttons
+	var options = Array.from(document.querySelectorAll("input[name = 'lect-pos']"));
+	options.forEach(option => {
+		option.addEventListener("click", function() {
+			setLecternPosition(option.value);
+		});
+	});
 
-	calcSize(); // quick testing
+	options = Array.from(document.querySelectorAll("input[name = 'lect-size']"));
+	options.forEach(option => {
+		option.addEventListener("click", function() {
+			setLecternSize(option.value);
+		});
+	});
+
+	options = Array.from(document.querySelectorAll("input[name = 'display']"));
+	options.forEach(option => {
+		option.addEventListener("click", function() {
+			setScreenType(option.value);
+		});
+	});
+
+	options = Array.from(document.querySelectorAll("input[name = 'tv-size']"));
+	options.forEach(option => {
+		option.addEventListener("click", function() {
+			setScreenSize(option.value);
+		});
+	});
+
+	pjPos.addEventListener("change", function() {
+		setProjectorPos();
+	});
+
+	dspPos.addEventListener("change", function() {
+		setDisplayPos();
+	});
+
+	// Create the three.js renderer and scene
+	createScene()
+
+	// Uncomment for testing
+	// dimX.value = 5;
+	// dimY.value = 4;
+	// dimZ.value = 2;
+
+	// setLecternSize("sml");
+	// setScreenType("pj");
+
+	// calcSize();
 
 });
 
 
 // Calculate room area and volume from user entered dimensions
 function calcSize() {
-
 	// Make sure X and Y have been entered correctly
 	if (dimX.value == "" || dimY.value == "" ) {
 		// Otherwise clear
 		document.getElementById("floor-area").innerHTML = "";
 		document.getElementById("room-volume").innerHTML = "";
+		drawRoom = false;
+		clearScene();
 		return;
 	}
 
@@ -48,6 +101,8 @@ function calcSize() {
 	// Don't calculate volume if Z hasn't been entered
 	if (dimZ.value == "") {
 		document.getElementById("room-volume").innerHTML = "";
+		drawRoom = false;
+		clearScene();
 		return;
 	}
 
@@ -55,97 +110,250 @@ function calcSize() {
 	document.getElementById("room-volume").innerHTML = volume + "m&sup3;";
 
 	// Draw the room when all values have been supplied
-	clearVis();
-	roomVisualiser();
-
+	drawRoom = true;
+	if (screenWidth == undefined) setScreenSize(55);
+	renderRoom();
 }
 
 
-// When the room values are changed, remove the
-// current visualiser before adding a new one
-function clearVis() {
-	var visChild = Array.from(visualiser.children);
+// Set the lectern size
+function setLecternSize(size) {
+	if (size == "sml") {
+		lectX = 1.200;
+		lectY = 1.022;
+		lectZ = 0.700;
+	}
 
-	visChild.forEach(child => {
-		child.remove();
-	});
+	else if (size = "lrg") {
+		lectX = 1.810;
+		lectY = 1.000;
+		lectZ = 0.700;
+	}
+
+	// If any dimension values are yet to be filled, don't draw the scene
+	if (drawRoom == false) return;
+	renderRoom();
 }
 
 
-// Draw a representation of the room to the canvas
-function roomVisualiser() {
-	const canvasWidth = visualiser.clientWidth;
-	const canvasHeight = visualiser.clientHeight;
+// Set the lectern position
+function setLecternPosition(pos) {
+	lectPos = pos;
+
+	if (drawRoom == false) return;
+	renderRoom();
+}
+
+
+// Set the screen type
+function setScreenType(type) {
+	screenType = type;
+
+	if (drawRoom == false) return;
+	renderRoom();
+}
+
+
+// Set the screen size
+function setScreenSize(diag) {
+	//Convert inches to metres
+	diag = diag * 0.0254;
+
+	// Find width and height from diagonal and aspect ratio
+	var aRWdth = 16;
+	var aRHght = 9;
+	var aRDiag = Math.sqrt(Math.pow(aRWdth, 2) + Math.pow(aRHght, 2));
+
+	var factor = diag / aRDiag;
+
+	screenWidth = aRWdth * factor;
+	screenHeight = aRHght * factor;
+
+	// If any dimension values are yet to be filled, don't draw the scene
+	if (drawRoom == false) return;
+	renderRoom();
+}
+
+
+// Redraw the room when projector distance slider changes
+function setProjectorPos() {
+	if (drawRoom == false) return;
+	renderRoom();
+}
+
+
+// Redraw the room when projector distance slider changes
+function setDisplayPos() {
+	if (drawRoom == false) return;
+	renderRoom();
+}
+
+
+//Create the three.js renderer and scene
+function createScene() {
+	renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+	scene = new THREE.Scene();
+	canvasWidth = visualiser.clientWidth;
+	canvasHeight = visualiser.clientHeight;
+
+	// Setup scene and renderer and add to the DOM
+	renderer.setSize(canvasWidth, canvasHeight);
+	renderer.setPixelRatio( window.devicePixelRatio ); // fix for HiDPI displays
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+	visualiser.appendChild(renderer.domElement);
+}
+
+
+// Update the room visualisation
+function renderRoom() {
 	const wallWidth = Number(dimX.value);
 	const wallHeight = Number(dimZ.value);
 	const roomDepth = Number(dimY.value);
+	var savedPosition;
 
-	// Setup scene and renderer and add to the DOM
-	const scene = new THREE.Scene();
+	// First, remove any other objects
+	clearScene();
 
-	const renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
-	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setPixelRatio( window.devicePixelRatio ); // fix for HiDPI displays
-
-	visualiser.appendChild(renderer.domElement);
-
-	// // Room wireframe
-	// var geometry = new THREE.BoxBufferGeometry(wallWidth, wallHeight, roomDepth);
-	// var edges = new THREE.EdgesGeometry(geometry);
-	// var material = new THREE.LineBasicMaterial( { color: 0x899197} );
-	// var wireframe = new THREE.LineSegments(edges, material);
-
-	// scene.add(wireframe);
-
-	// Back wall
-	var geometry = new THREE.PlaneGeometry(wallWidth, wallHeight);
-	var material = new THREE.MeshStandardMaterial ( { color: 0xf0f1f2 } );
+	// Create a cube with material on the inside only, gives the
+	// effect of looking through outer walls to inside of room
+	var geometry = new THREE.BoxBufferGeometry(wallWidth, wallHeight, roomDepth);
+	var material = new THREE.MeshStandardMaterial ( { color: 0xf0f1f2, side: THREE.BackSide } );
 	var mesh = new THREE.Mesh (geometry, material);
 
-	mesh.position.z = roomDepth / -2;
+	scene.add(mesh);
+
+	// Draw identical cube but with a shadow receiving material
+	material = new THREE.ShadowMaterial( { side: THREE.BackSide } );
+	material.opacity = 0.05;
+	mesh = new THREE.Mesh (geometry, material);
+	mesh.receiveShadow = true;
 
 	scene.add(mesh);
 
-	// Side wall
-	geometry = new THREE.PlaneGeometry(roomDepth, wallHeight);
+
+	// Draw display if one has been selected
+	if (screenType != undefined) {
+		// Calculate X offset for later
+		var wallPos = (wallWidth / -2) + (wallWidth * (dspPos.value / 100));
+
+		if (dspPos.value < 50) {
+			wallPos += (screenWidth / 2) * ((100 - dspPos.value) / 100);
+		} else if (dspPos.value > 50) {
+			wallPos -= (screenWidth / 2) * (dspPos.value / 100);
+		}
+
+		// Make a dark screen for a TV
+		if (screenType == "tv") {
+			screenColor = 0x757f86;
+			screenThick = 0.07;
+		}
+
+		// Make a light screen and box for a projector
+		else if (screenType == "pj") {
+			screenColor = 0xffffff;
+			screenThick = 0.02;
+
+			// Draw the projector
+			geometry = new THREE.BoxBufferGeometry( 0.8, 0.2, 0.5 );
+			material = new THREE.MeshStandardMaterial ( { color: 0x757f86 } );
+			mesh = new THREE.Mesh (geometry, material);
+
+			// Initially position projector to ceiling and front of room
+			mesh.position.y = (wallHeight / 2) - (0.2 / 2);
+			mesh.position.z = (roomDepth / -2) + (0.5 / 2);
+
+			// Offset Z position
+			var pjCeilingDist = 0.1;
+			var pjMaxDist = roomDepth - 0.5;
+			var pjScreenDist = pjMaxDist * (1 / (100 / -(pjPos.value - 100)));
+
+			mesh.position.x = wallPos;
+			mesh.position.y -= pjCeilingDist;
+			mesh.position.z += pjScreenDist;
+			mesh.castShadow = true;
+
+			scene.add(mesh);
+		}
+
+		// The wall mounted screen
+		geometry = new THREE.BoxBufferGeometry(
+			screenWidth,
+			screenHeight,
+			screenThick
+		);
+		material = new THREE.MeshStandardMaterial ( { color: screenColor } );
+		mesh = new THREE.Mesh (geometry, material);
+
+		mesh.position.x = wallPos;
+		mesh.position.y = wallHeight / 10;
+		mesh.position.z = (roomDepth / -2) + 0.05;
+
+		mesh.castShadow = true;
+
+		savedPosition = mesh.position;
+		scene.add(mesh);
+
+		// For shadows on the display
+		material = new THREE.ShadowMaterial();
+		material.opacity = 0.05;
+		mesh = new THREE.Mesh (geometry, material);
+
+		mesh.position.set(savedPosition.x, savedPosition.y, savedPosition.z);
+		mesh.receiveShadow = true;
+
+		scene.add(mesh);
+
+	}
+
+
+	// Draw a lectern
+	geometry = new THREE.BoxBufferGeometry( lectX, lectY, lectZ );
+	material = new THREE.MeshStandardMaterial ( { color: 0x868375 } );
 	mesh = new THREE.Mesh (geometry, material);
 
-	mesh.position.x = wallWidth / -2;
-	mesh.rotateY(90 * Math.PI / 180);
+	// Move lectern to front of room
+	var sittingGap = 0.7;
+	mesh.position.y = (wallHeight / -2) + (lectY / 2);
+	mesh.position.z = (roomDepth / -2) + (lectZ / 2) + sittingGap;
 
+	// Position left or right
+	var wallGap = 0.1;
+	if (lectPos == "left") {
+		mesh.position.x = (wallWidth / -2) + (lectX / 2) + wallGap;
+	}
+	else if (lectPos == "right") {
+		mesh.position.x = (wallWidth / 2) - (lectX / 2) - wallGap;
+	}
+	mesh.castShadow = true;
+
+	savedPosition = mesh.position;
 	scene.add(mesh);
 
-	// Floor
-	geometry = new THREE.PlaneGeometry(wallWidth, roomDepth);
-	// material = new THREE.MeshStandardMaterial ( { color: 0xb9bec2 } );
+	// Shadow receiver
+	material = new THREE.ShadowMaterial();
+	material.opacity = 0.05;
 	mesh = new THREE.Mesh (geometry, material);
 
-	mesh.position.y = wallHeight / -2;
-	mesh.rotateX(-90 * Math.PI / 180);
+	mesh.position.set(savedPosition.x, savedPosition.y, savedPosition.z);
+	mesh.receiveShadow = true;
 
 	scene.add(mesh);
 
-	// Display
-	const aspect = 1.78;
-	geometry = new THREE.BoxBufferGeometry((wallHeight / 2) * aspect, (wallHeight / 2), 0.05)
-	material = new THREE.MeshStandardMaterial ( { color: 0x757f86 } );
-	mesh = new THREE.Mesh (geometry, material);
-
-	mesh.position.z = (roomDepth / -2) + 0.05;
-	mesh.position.y = wallHeight / 10;
-
-	scene.add(mesh);
 
 	// Light the scene
-	const intensity = 1.5;
-	const light = new THREE.DirectionalLight(0xffffff, intensity);
+	const lightIntensity = 1.5;
+	var light = new THREE.DirectionalLight(0xffffff, lightIntensity);
 	light.position.set(1, 1.25, 1.1);
+	light.castShadow = true;
 
-	scene.add(light)
+	scene.add(light);
 
 	// // Debug lights
 	// var helper = new THREE.DirectionalLightHelper(light, 1, 0xb5dde1);
 	// scene.add(helper);
+
 
 	// Camera setup
 	var camera = new THREE.OrthographicCamera(
@@ -157,17 +365,6 @@ function roomVisualiser() {
 		1000
 	);
 
-	// Zoom the camera to fit the room based on the longest edge
-	if (wallWidth > wallHeight && wallWidth > roomDepth) {
-		camera.zoom = canvasWidth / (wallWidth * 2);
-	} else if (roomDepth > wallHeight) {
-		camera.zoom = canvasWidth / (roomDepth * 2);
-	} else {
-		camera.zoom = canvasHeight / (wallHeight * 2);
-	}
-
-	camera.updateProjectionMatrix();
-
 	// Move and adjust the camera angle
 	camera.position.x = 45;
 	camera.position.y = 30;
@@ -175,14 +372,20 @@ function roomVisualiser() {
 
 	camera.lookAt( new THREE.Vector3(0, 0, 0) );
 
+	// Zoom camera to fit the diagonal of the room
+	var floorDiag = Math.sqrt(Math.pow(wallWidth, 2) + Math.pow(roomDepth, 2));
+	camera.zoom = (canvasWidth - 50) / floorDiag;
+
+	camera.updateProjectionMatrix();
+
+
 	// Render the static scene
-	// Uncomment lines if animation updating is needed
+	renderer.render(scene, camera);
+}
 
-	// function animate() {
-		// requestAnimationFrame(animate);
-		renderer.render(scene, camera);
-	// }
 
-	// animate();
-
+function clearScene() {
+	while (scene.children.length > 0) {
+		scene.remove(scene.children[0]);
+	}
 }
